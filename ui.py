ad464eef00a79fb978df222dc4c2a1176ec1b25d
zyzlik <andreychukk@mail.ru>
2017-01-15 18:37:15 -0600

    add Bank class, realize loans and deposits

--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf8 -*-
 
 import curses
+import random
 import time
 
 from collections import OrderedDict
@@ -24,24 +25,91 @@ NO = 0
 
 INITIAL_LOAN_RATE = 10
 INITIAL_DEPOSIT_RATE = 8
+RATE_SCATTER = (5, 15)
 INITIAL_INCOME_TAX = 5
 INITIAL_REPLACEMENT_COST = 19
 
+KEY_0 = 48
+KEY_1 = 49
 
-class User(object):
+
+class Bank(Observer):
+
+    def __init__(self):
+        self.loan_rate = INITIAL_LOAN_RATE
+        self.deposit_rate = INITIAL_DEPOSIT_RATE
+
+    def update(self, date):
+        if date.day == 1:
+            self.loan_rate = random.randint(*RATE_SCATTER)
+            self.deposit_rate = random.randint(*RATE_SCATTER)
+
+    def update_deposits(self, deposits):
+        for month, money in deposits.items():
+            deposits[month] = money + money * self.deposit_rate / 100
+
+    def update_loans(self, loans):
+        for month, money in loans.items():
+            loans[month] = money + money * self.loan_rate / 100
+
+
+class User(Observer):
 
     def __init__(self, name):
         self.name = name
         self.scores = 0
-        self.money = 0
+        self.total_money = 30000
         self.property = {
-            apt: None,
-            car: None,
-            oil: None,
-            land: None,
+            'apt': None,
+            'car': None,
+            'oil': None,
+            'land': None,
         }
         self.marriage = False
         self.sick = False
+        self.deposits = {
+            1: 10000,
+            5: 15500
+        }
+        self.loans = {
+            6: 12200
+        }
+        self.bank = Bank()
+        self.month = None
+
+    def update(self, date):
+        self.month = date.month
+        if date.day == 1:
+            if date.month in self.deposits:
+                self.get_payment(date.month)
+            if date.month in self.loans:
+                self.pay_loan(date.month)
+            self.bank.update_deposits(self.deposits)
+            self.bank.update_loans(self.loans)
+
+    def get_payment(self, month):
+        self.total_money += self.deposits.pop(month)
+
+    def pay_loan(self, month):
+        self.total_money -= self.loans.pop(month)
+
+    def new_deposit(self, amount, term):
+        if amount < self.total_money:
+            month = (self.month + term) % 12
+            self.deposits.update({month: amount})
+            self.total_money -= amount
+            return True
+        else:
+            return False
+
+    def new_loan(self, amount, term):
+        if amount < self.total_money:
+            month = (self.month + term) % 12
+            self.loans.update({month: amount})
+            self.total_money += amount
+            return True
+        else:
+            return False
 
 
 class DateCounter(Observable):
@@ -70,11 +138,12 @@ class Screen(object):
         curses.def_prog_mode()
 
         self.screen.bkgd(' ', curses.color_pair(1))
+        self.user = User('Ksenia')
         self.menu = MenuPanel(self)
         self.date = DatePanel(self.side_panel_width)
         self.tax = TaxPanel(self.side_panel_width)
-        self.finance = FinancePanel(self.width)
-        self.bank = BankPanel(self.width, self.height)
+        self.finance = FinancePanel(self.width, self.user)
+        self.bank = BankPanel(self.width, self.height, self.user)
 
         self.options = {
             curses.KEY_F1: self.show_bank,
@@ -92,7 +161,6 @@ class Screen(object):
 
     def show_bank(self):
         self.bank.draw()
-        self.bank.draw_choice()
 
     def show_market(self):
         wind = self.draw_event_window()
@@ -209,20 +277,19 @@ class TaxPanel(Observer):
         self.panel.addstr(4, 2, replacement_cost_str)
         self.panel.refresh()
 
-    def update(self, msg):
-        pass
+    def update(self, date):
+        if date.day == 1:
+            self.draw()
 
 
-class FinancePanel(object):
+class FinancePanel(Observer):
 
-    def __init__(self, main_width):
-        self.user_money = 0
-        self.user_deposit = 0
-        self.user_loan = 0
-        self.month_income = 0
-        self.house_rate = 0
-        self.land_rate = 0
+    def __init__(self, main_width, user):
+        self.user = user
         self.panel = curses.newwin(9, main_width // 2 - 1, 2, main_width // 2)
+        self.month_income = 0
+        self.house_rate = 15
+        self.land_rate = 10
         self.draw()
 
     def draw(self):
@@ -230,9 +297,9 @@ class FinancePanel(object):
         self.panel.bkgd(' ', curses.color_pair(4))
         self.panel.box(curses.ACS_VLINE, curses.ACS_HLINE)
         height, width = self.panel.getmaxyx()
-        money_str = 'У вас на счету: %s %s' % (self.user_money, human_money(self.user_money))
-        deposit_str = 'Вам должны: %s %s' % (self.user_deposit, human_money(self.user_deposit))
-        loan_str = 'Вы должны: %s %s' % (self.user_loan, human_money(self.user_loan))
+        money_str = 'У вас на счету: %s %s' % (self.user.total_money, human_money(self.user.total_money))
+        deposit_str = 'Вам должны: %s %s' % (sum(self.user.deposits.values()), human_money(sum(self.user.deposits.values())))
+        loan_str = 'Вы должны: %s %s' % (sum(self.user.loans.values()), human_money(sum(self.user.loans.values())))
         month_income_str = 'Итого прибыль: %s %s' % (self.month_income, human_money(self.month_income))
         house_rate_str = 'Плата за дом: %s%%' % self.house_rate
         land_rate_str = 'Плата за землю: %s%%' % self.land_rate
@@ -245,13 +312,15 @@ class FinancePanel(object):
         self.panel.addstr(7, 2, land_rate_str)
         self.panel.refresh()
 
+    def update(self, date):
+        self.draw()
+
 
 class BankPanel(object):
 
-    def __init__(self, main_width, main_height):
+    def __init__(self, main_width, main_height, user):
         self.panel = curses.newwin(main_height // 2, main_width - 8, main_height // 2 - 4, 4)
-        self.plus = {1: 10000, 4: 12000, 7: 11000, 12: 4000}
-        self.minus = {6: 5000}
+        self.user = user
 
     def draw(self):
         self.panel.clear()
@@ -278,27 +347,32 @@ class BankPanel(object):
                         return 4
                     return 5
 
-            if m in self.plus or m + 6 in self.plus:
-                for month, money in self.plus.items():
+            if m in self.user.deposits or m + 6 in self.user.deposits:
+                for month, money in self.user.deposits.items():
                     if month % 6 == m:
-                        self.panel.addstr(row(month), i, str(self.plus[month]))
+                        self.panel.addstr(row(month), i, str(money))
                     elif month % 6 == 0 and m == 6:
-                        self.panel.addstr(row(month), i, str(self.plus[month]))
+                        self.panel.addstr(row(month), i, str(money))
             else:
                 self.panel.addstr(2, i, str(0))
                 self.panel.addstr(3, i, str(0))
 
-            if m in self.minus or m + 6 in self.minus:
-                for month, money in self.minus.items():
+            if m in self.user.loans or m + 6 in self.user.loans:
+                for month, money in self.user.loans.items():
                     if month % 6 == m:
-                        self.panel.addstr(row(month, False), i, str(self.minus[month]))
+                        self.panel.addstr(row(month, False), i, str(money))
                     elif month % 6 == 0 and m == 6:
-                        self.panel.addstr(row(month, False), i, str(self.minus[month]))
+                        self.panel.addstr(row(month, False), i, str(money))
             else:
                 self.panel.addstr(4, i, str(0))
                 self.panel.addstr(5, i, str(0))
             m += 1
+        self.panel.addstr(6, 2, '_' * (width - 4))
         self.panel.refresh()
+        self.panel.nodelay(NO)
+        key = self.panel.getch()
+        if key:
+            self.draw_choice()
 
     def draw_choice(self):
         height, width = self.panel.getmaxyx()
@@ -312,6 +386,41 @@ class BankPanel(object):
         choice_panel.addstr(2, width // 2 - len(question) // 2, question)
         self.panel.refresh()
         choice_panel.refresh()
+        choice_panel.nodelay(NO)
+        key = choice_panel.getch()
+        if key == KEY_0:
+            choice_panel.erase()
+            del choice_panel
+            self.panel.refresh()
+            self.panel.addstr(7, 2, 'Какую сумму вы хотели бы дать? ')
+            curses.echo()
+            amount = self.panel.getstr()
+            if self.validate_int(amount):
+                self.panel.addstr(8, 2, 'На какой срок (1-11 месяцев)? ')
+                curses.echo()
+                term = self.panel.getstr()
+                if self.validate_int(term) and self.validate_month(term):
+                    self.user.new_deposit(int(amount), int(term))
+            self.hide()
+        if key == KEY_1:
+            del choice_panel
+            self.panel.addstr(7, 2, 'Какую сумму вы хотели бы взять?')
+
+    def hide(self):
+        self.panel.erase()
+        self.panel.refresh()
+        del self.panel
+
+    def validate_int(self, input):
+        if input.isdigit():
+            return True
+        return False
+
+    def validate_month(self, input):
+        if 11 >= int(input) >= 1:
+            return True
+        else:
+            return False
 
 
 if __name__ == '__main__':
@@ -320,6 +429,9 @@ if __name__ == '__main__':
     screen = Screen()
     date_counter.register(screen.date)
     date_counter.register(screen.tax)
+    date_counter.register(screen.user)
+    date_counter.register(screen.user.bank)
+    date_counter.register(screen.finance)
     count = 10
     while count != 0:
         time.sleep(1)
